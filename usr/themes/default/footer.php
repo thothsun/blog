6cf2c7366bf26ccd63221fe6302675fb5d738c1e
sunshuai <438819781@qq.com>
2018-12-20 16:17:14 +0800

    添加备案信息

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -5,9 +5,12 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
+    <?php _e('<a href = "http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=21011202000327">辽公网安备 21011202000327号</a>'); ?>
+    <?php _e('|'); ?>
+    <?php _e('冀ICP备16021363号-4'); ?>
+    <?php _e('|'); ?>
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('All rights reserved.'); ?>
-
     <br/>
     <?php _e('Powered by <a href="http://www.typecho.org">Typecho</a>'); ?>.
     <?php _e('Theme by <a href="https://github.com/Sun-Shuai/blog">SunShuai</a>'); ?>.
